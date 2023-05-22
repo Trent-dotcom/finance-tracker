7e83a4361c83dc4e743ca155009cc68286e5fa3f
Trent Livingston <76966018+Trent-dotcom@users.noreply.github.com>
2023-05-22 07:24:18 -0600

    Added state to my form submissions

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,21 +1,31 @@
 import './ExpenseForm.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 const ExpenseForm = () => {
+
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
+
+    const titleChangeHandler = (e) => { setUserInput({ ...userInput, enteredTitle: e.target.value }); };
+    const amountChangeHandler = (e) => { setUserInput({ ...userInput, enteredAmount: e.target.value }); };
+    const dateChangeHandler = (e) => { setUserInput({ ...userInput, enteredDate: e.target.value }); };
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2008-01-01' />
+                    <input type='date' min='2008-01-01' onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className='new-expense__actions'>
